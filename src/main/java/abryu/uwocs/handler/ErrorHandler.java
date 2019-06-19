3ac078641c4cfe85fa9e3569ea89d9b501fad81b
abryu <wenboyu2208@gmail.com>
2019-06-19 12:30:40 -0400

    update error handler

--- src/main/java/abryu/uwocs/handler/ErrorHandler.java
+++ src/main/java/abryu/uwocs/handler/ErrorHandler.java
@@ -1,19 +1,21 @@
 package abryu.uwocs.handler;
 
+import com.amazon.ask.dispatcher.exception.ExceptionHandler;
 import com.amazon.ask.dispatcher.request.handler.HandlerInput;
 import com.amazon.ask.dispatcher.request.handler.RequestHandler;
 import com.amazon.ask.model.Response;
 
 import java.util.Optional;
 
-public class ErrorHandler implements RequestHandler {
+public class ErrorHandler implements ExceptionHandler {
+
   @Override
-  public boolean canHandle(HandlerInput handlerInput) {
+  public boolean canHandle(HandlerInput handlerInput, Throwable throwable) {
     return true;
   }
 
   @Override
-  public Optional<Response> handle(HandlerInput handlerInput) {
+  public Optional<Response> handle(HandlerInput handlerInput, Throwable throwable) {
     final String speechOutput = "Error happened ; Please check Cloud Watch Logs";
     return handlerInput.getResponseBuilder().withSpeech(speechOutput).withReprompt(speechOutput).build();
   }
